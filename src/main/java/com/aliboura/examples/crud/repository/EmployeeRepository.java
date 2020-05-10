5f0de33e70a4e3aab3b135fbff10b1944ee38efe
aliboura <aliboura@gmail.com>
2020-05-10 10:38:10 +0100

    first commit

---
+++ src/main/java/com/aliboura/examples/crud/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.aliboura.examples.crud.repository;
+
+import com.aliboura.examples.crud.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
