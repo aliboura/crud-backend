6f4c4c797c2bddb948f29a08c36681e3515358db
aliboura <aliboura@gmail.com>
2020-05-10 10:06:54 +0100

    first commit

---
+++ src/test/java/com/aliboura/examples/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliboura.examples.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
